828710b99ccb0d4fbec7050e58fd226d6194506a
rudnicka <gosia.stempin@gmail.com>
2015-06-24 15:36:33 +0200

    Rozwiązanie euler1

--- euler1.py
+++ euler1.py
@@ -5,13 +5,15 @@ If we list all the natural numbers below 10 that are multiples of 3 or
 Find the sum of all the multiples of 3 or 5 below 1000
 """
 
-def euler1():
+def euler1(n):
     """
-    >>> euler1()
+    >>> euler1(10)
     23
     """
-    pass
+    return sum(i for i in range(1, n) if i % 3 == 0 or i % 5 == 0)
 
 if __name__ == "__main__":
+    print euler1(10)
+    print euler1(1000)
     import doctest
     doctest.testmod()
